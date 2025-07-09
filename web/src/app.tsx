668906ec78b889b392ce5a2920bf59a497f5e2e7
João Paulo Godinho <j_paulo_g@hotmail.com>
2025-07-09 20:30:37 -0300

    finalizando a aula 1 com setup da aplicação web conectando com a api

---
+++ web/src/app.tsx
@@ -0,0 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CreateRoom } from './pages/ceate-room';
+import { Room } from './pages/room';
+
+export function App() {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoom />} index />
+
+          <Route element={<Room />} path="/room/:roomId" />
+
+          <Route />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
